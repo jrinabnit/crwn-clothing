ba5a4014fb046ee7e610b61170d05f53243072e3
abc <abc@example.com>
2020-10-20 13:02:12 +0800

    implement code to pull in shop data from firestore and store it into the shop reducer from our shop page. Added spinner and updated buttons with stylized components.

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,49 +3,78 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyDYWzHOoTRGxo_HsfR7hxogVzrjt__Tzkw",
-    authDomain: "crwn-db-e970e.firebaseapp.com",
-    databaseURL: "https://crwn-db-e970e.firebaseio.com",
-    projectId: "crwn-db-e970e",
-    storageBucket: "crwn-db-e970e.appspot.com",
-    messagingSenderId: "457220305745",
-    appId: "1:457220305745:web:3968f63b2d7116c77a9970",
-    measurementId: "G-V2B3DGTZMS"
-  }
+	apiKey: 'AIzaSyDYWzHOoTRGxo_HsfR7hxogVzrjt__Tzkw',
+	authDomain: 'crwn-db-e970e.firebaseapp.com',
+	databaseURL: 'https://crwn-db-e970e.firebaseio.com',
+	projectId: 'crwn-db-e970e',
+	storageBucket: 'crwn-db-e970e.appspot.com',
+	messagingSenderId: '457220305745',
+	appId: '1:457220305745:web:3968f63b2d7116c77a9970',
+	measurementId: 'G-V2B3DGTZMS'
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if (!userAuth) return;
-  
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
-  
-    const snapShot = await userRef.get();
-  
-    if (!snapShot.exists) {
-      const { displayName, email } = userAuth;
-      const createdAt = new Date();
-      try {
-        await userRef.set({
-          displayName,
-          email,
-          createdAt,
-          ...additionalData
-        });
-      } catch (error) {
-        console.log('error creating user', error.message);
-      }
-    }
-  
-    return userRef;
-  };
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) return;
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({ prompt: 'select_account'});
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+	const snapShot = await userRef.get();
 
-  export default firebase;
-  
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData
+			});
+		} catch (error) {
+			console.log('error creating user', error.message);
+		}
+	}
+
+	return userRef;
+};
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+	const collectionRef = firestore.collection(collectionKey);
+
+	const batch = firestore.batch();
+	objectsToAdd.forEach((obj) => {
+		const newDocRef = collectionRef.doc();
+		batch.set(newDocRef, obj);
+	});
+
+	return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collection) => {
+	const transformedCollection = collection.docs.map((doc) => {
+		const { title, items } = doc.data();
+
+		return {
+			routeName: encodeURI(title.toLowerCase()),
+			id: doc.id,
+			title,
+			items
+		};
+	});
+
+	return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  } , {})
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Directory from '../../components/directory/directory.component.jsx';
 
-import './homepage.styles.scss';
+import { HomePageContainer } from './homepage.styles';
 
 const HomePage = () => (
-	<div className="homepage">
-		<Directory />
-	</div>
+	<HomePageContainer>
+<Directory />
+	</HomePageContainer>
+		
+
 );
 
 export default HomePage;

5a7e5053867662fdf7e672a99be2c82a44028565
jrinabnit <jrinabnit@gmail.com>
2020-11-02 12:49:02 +0800

    updated application to be mobile responsive

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,13 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
-
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
-
 import Header from './components/header/header.component';
+
+import { GlobalStyle } from './global.styles';
 
 import { checkUserSession } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
@@ -25,6 +24,7 @@ const App = ({ checkUserSession }) => {
 
 	return (
 		<div>
+			<GlobalStyle />
 			<Header />
 			<Switch>
 				<Route exact path="/" component={HomePage} />
